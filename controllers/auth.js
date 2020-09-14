76dfa96ca5d7705de67bd8650fe5201cf96288e5
Edward Gillard <edwardgillard@edwards-mbp.mynet>
2020-09-14 16:44:31 +0100

    updated code for register

--- controllers/auth.js
+++ controllers/auth.js
@@ -27,7 +27,7 @@ async function login(req, res, next) {
     const user = await User.findOne({ email: req.body.email })
     if (!user || !user.validatePassword(req.body.password)) throw new Error(unauthorized)
     const token = jwt.sign({ sub: user._id }, secret, { expiresIn: '7 days' })
-    res.status(202).json({ message: `Hello ${user.username}`, token })
+    res.status(202).json({ message: `Hello ${user.userName}`, token })
   } catch (err) {
     next(err)
   }
