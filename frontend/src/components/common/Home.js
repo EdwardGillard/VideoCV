020457ccf686007260553164cb8d5a900b4e5f8d
tly1001 <thomasly1992@gmail.com>
2020-09-16 18:57:58 +0100

    done navbar

--- frontend/src/components/common/Home.js
+++ frontend/src/components/common/Home.js
@@ -2,21 +2,12 @@ import React from 'react'
 import Navbar from '../common/Navbar'
 
 function Home () {
-  const handleScroll = () => {
-    console.log(window.pageYOffset)
-    if (window.pageYOffset > 200 ) {
-      setNavScroll('nav-colored')
-    } else {
-      setNavScroll('nav-transparent')
-    }
-  }
+
 
   return (
     <>
-      <body>
-        <Navbar/>
-        <h1>Hello World</h1>
-      </body>
+      <Navbar/>
+      <h1>Hello World</h1>
     </>
   )
 }
--- frontend/src/components/common/Navbar.js
+++ frontend/src/components/common/Navbar.js
@@ -5,26 +5,42 @@ function Navbar() {
   const [navScroll, setNavScroll] = React.useState('nav-transparent')
 
   useEffect(() => {
-
+    function onScroll() {
+      window.pageYOffset > 50 ? setNavScroll('nav-colored') : setNavScroll('nav-transparent')
+    }
+    window.addEventListener('scroll', onScroll)
   }, [])
 
   return (
     <>
-      <nav id="nav" onScroll={handleScroll} className={navScroll}>
-        <img src="./assets/logo.png" alt="Logo" width="80" height="80"/>
-        <ul className="nav-links">
-          <li><a className="nav-link" href="/index.html">Home</a></li>
-          <li><a className="nav-link" href="/mission.html">The Mission</a></li>
-          <li className="nav-link dropdown"> Services
-            <div className="dropdown-content">
-              <a href="/branding.html">Branding</a>
-              <a href="/web-dev.html">Web Development</a>
-              <a href="/else.html">Something Else</a>
-              <a href="/marketing.html">Marketing</a>
-            </div>
-          </li>
-          <li><a className="nav-link" href="/contact.html">Contact</a></li>
-        </ul>
+      <nav id="nav" className={navScroll}>
+
+        <div className='nav-left'>
+          <img src={require('../../assets/videocv-logo.png')} alt="Logo" width="80" height="40"/>
+          <ul className="nav-links">
+            <li><a className="nav-link" href="/index.html">Home</a></li>
+            <li><a className="nav-link" href="/profiles.html">Profiles</a></li>
+            <li className="nav-link dropdown"> Categories
+              <div className="dropdown-content">
+                <a href="/">Projects</a>
+                <a href="/">Personal</a>
+              </div>
+            </li>
+          </ul>
+        </div>
+
+        <div className='nav-right'>
+          <ul>
+            <li className="nav-link dropdown settings"> ⚙
+              <div className="dropdown-content">
+                <a href="/">My profile</a>
+                <a href="/">login</a>
+                <a href="/">logout</a>
+              </div>
+            </li>
+          </ul>
+        </div>
+
       </nav>
     </>
   )
